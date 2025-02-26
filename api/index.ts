d3050b61ee6b8244c4a205c09919139a658fb185
Alf <42708476+alfset@users.noreply.github.com>
2025-02-27 02:21:00 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://surabaya-blockchain-alliance-ibc0nzfl6-alfsets-projects.vercel.app', 'https://x.com'],
+  origin: ['http://localhost:3000', 'https://surabaya-blockchain-alliance-sand.vercel.app', 'https://x.com'],
   credentials: true,
 }));
 
@@ -23,7 +23,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax', // Changed from 'None' to 'lax' for better localhost compatibility
     maxAge: 60 * 60 * 1000,
@@ -157,7 +157,7 @@ app.get('/connect/twitter/callback', async (req, res) => {
         });
       });
   
-      res.redirect('https://surabaya-blockchain-alliance-ibc0nzfl6-alfsets-projects.vercel.app/setup');
+      res.redirect('https://surabaya-blockchain-alliance-sand.vercel.app/setup');
     } catch (error) {
       console.error('Error in Twitter callback:', error);
       res.status(500).json({ error: 'Error completing Twitter authentication' });
@@ -179,7 +179,7 @@ app.get('/connect/twitter/callback', async (req, res) => {
 const discordOAuth = {
   clientId: process.env.DISCORD_CLIENT_ID,
   clientSecret: process.env.DISCORD_CLIENT_SECRET,
-  redirectUri: 'http://localhost:5000/connect/discord/callback',
+  redirectUri: 'https://bakcend-surabaya-blockchain-aliance.vercel.app/connect/discord/callback',
 };
 
 app.get('/connect/discord', (req, res) => {
@@ -220,7 +220,7 @@ app.get('/connect/discord/callback', async (req, res) => {
       accessToken: access_token
     };
 
-    res.redirect('https://surabaya-blockchain-alliance-ibc0nzfl6-alfsets-projects.vercel.app/setup');
+    res.redirect('https://surabaya-blockchain-alliance-sand.vercel.app/setup');
   } catch (error) {
     console.error('Error connecting to Discord:', error.response ? error.response.data : error.message);
     res.status(500).send('Error connecting to Discord');

33ff7b82a9ba3684f92f060a1c0c1b19492e09b4
Alf <42708476+alfset@users.noreply.github.com>
2025-02-27 12:48:19 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,8 +13,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: ['https://surabaya-blockchain-alliance-sand.vercel.app', 'https://x.com'],
-  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Access-Token'], 
+  credentials: true, 
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
